8510682d98580567b66ac0d9b1d846eefb863a4d
ali <zailikhan72@gmail.com>
2026-02-22 22:54:21 +0500

    solution added

---
+++ 344. Reverse String.py
@@ -0,0 +1,40 @@
+# DESCRIPTION
+
+    """
+    
+    Write a function that reverses a string. The input string is given as an array of characters s.
+
+You must do this by modifying the input array in-place with O(1) extra memory.
+
+ 
+
+Example 1:
+
+Input: s = ["h","e","l","l","o"]
+Output: ["o","l","l","e","h"]
+Example 2:
+
+Input: s = ["H","a","n","n","a","h"]
+Output: ["h","a","n","n","a","H"]
+ 
+    
+    """
+    
+# SOLTION
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        n = len(s)
+        l = 0
+        r = n-1
+
+        while l < r:
+            s[l],s[r] = s[r],s[l]
+            l += 1
+            r -= 1
+
+        # time : O(n)
+        # spave: O(1)
